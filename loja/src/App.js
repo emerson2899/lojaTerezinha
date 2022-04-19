9e54bf60ab2d483fbe4d2981b914989f276f0a3a
Emerson Felipe <felipemartins@gmail.com>
2022-04-19 20:18:40 -0300

    adicao do carrinho

--- loja/src/App.js
+++ loja/src/App.js
@@ -9,6 +9,7 @@ import MenuItem from "./MenuItem";
 import { Home } from "./pages/Home/Home";
 import Vendas from "./pages/Vendas/Vendas";
 import Clientes from "./pages/Clientes/Clientes";
+import Car from "./pages/Car/Car";
 
 
 
@@ -40,6 +41,9 @@ function App() {
       <li>
         <Link to='/User'>Perfil</Link>
       </li>
+      <li>
+        <Link to='/Car'>Carrinho</Link>
+      </li>
     </ul>
   </nav>
  
@@ -59,6 +63,7 @@ function App() {
       <Route component={Vendas} path='/Vendas' />
       <Route component={Clientes} path='/Clientes' />
       <Route component={User} path='/User' />
+      <Route component={Car} path='/Car'/>
 
 
       
---
+++ loja/src/pages/Car/Car.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Conteudo, Titulo } from "../../estilo";
+
+function Car (){
+
+    return(<>
+
+    <Conteudo>
+    <Titulo> < h1 className="titulo">Loja</h1> <br/> </Titulo>
+
+    <h3>Itens selecionados</h3>
+
+    </Conteudo>
+    
+    
+    </>);
+}
+ export default Car
