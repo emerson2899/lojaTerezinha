76419d89ae5c02915e8efd2de38086730e0c594b
Emerson Felipe <felipemartins@gmail.com>
2022-04-24 10:55:36 -0300

    midifi fornt

--- loja/src/pages/Home/Home.jsx
+++ loja/src/pages/Home/Home.jsx
@@ -6,7 +6,7 @@ export const Home = () => {
     return(
         <>
         <Titulo> < h1 className="titulo">Loja</h1> <br/> </Titulo>
-        <h3>Home</h3>
+        <h3>Home3</h3>
         </>
     );
 }
